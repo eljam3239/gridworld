a456254075a2d1686aae848a139523f45631f4ad
eljam3239 <elijames2301@gmail.com>
2025-02-16 23:05:07 -0500

    Write return statement to solve_linear_system

--- a1.py
+++ a1.py
@@ -200,7 +200,7 @@ class GridWorld:
                 for action in self.get_actions(state):
                     transitions = self.get_transitions(state, action)
                     action_value = 0
-                    for transition in transition:
+                    for transition in transitions:
                         action_value += transition['prob'] * (self.get_reward(state) + discount_factor * self.get_value(transition['state']))
                     if action_value > new_value:
                         new_value = action_value
@@ -210,6 +210,7 @@ class GridWorld:
             self.set_next_values()
             if max_diff < 0.01:
                 break
+        return self._grid_values
 
     def __str__(self):
         """
